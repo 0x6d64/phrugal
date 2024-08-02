93e3957e78fd7d48e727f645e28744cf03ce9a43
Martin Demling <0x6d64@martin-demling.de>
2024-08-02 17:51:23 +0300

    extend notes on how to implement

--- src/phrugal/composer.py
+++ src/phrugal/composer.py
@@ -2,13 +2,24 @@ class PhrugalComposer:
     def __init__(self, input_files):
         pass
 
-    def create_compositions(self):
+    def create_compositions(self, images_count: int):
         """Does the following:
 
         - read the image metadata of the input files
         - figure out the images that go together in a single composition
         - for each composition: create it, and write the output into the output folder
         - after that, free the space of that composition
+
+        - For each picture in the input, calculate the aspect ratio
+            - if the aspect ratio is >1, calculate 1/aspect ratio (to normalize it)
+        - sort the images by aspect ratio
+        - do groups for N images
+        - for each of the groups:
+            - do all combinations of orders of images
+            - for each combination: create score for the fit
+                - todo: how can we assess the fit as a score?
+            - then: select best, trigger actual creation of the image
+        
 
         """
         pass
