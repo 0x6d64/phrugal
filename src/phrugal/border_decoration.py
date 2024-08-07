70c8b2da1b535b44021228f7666878f17b95c8b8
Martin Demling <0x6d64@martin-demling.de>
2024-08-07 08:56:48 +0300

    start refactoring image decoration

--- src/phrugal/border_decoration.py
+++ src/phrugal/border_decoration.py
@@ -1,24 +1,79 @@
 from typing import Optional
 
+from PIL.Image import Image
 from PIL.ImageColor import getrgb
 
+from .image import PhrugalImage
 from .types import ColorTuple, Dimensions
 
 
-class BorderDecoration:
+class BorderDecorator:
     """Represents geometry of an image border and the text written on it"""
 
     TEXT_RATIO = 0.7  # how many percent of the border shall be covered by text
 
     def __init__(
         self,
+        base_image: PhrugalImage,
         background_color: str = "white",
         text_color: str = "black",
         font: Optional[str] = None,
     ):
+        self.base_image = base_image
         self.background_color = getrgb(background_color)  # type: ColorTuple
         self.text_color = getrgb(text_color)  # type: ColorTuple
         self.font = font
+
+    def get_decorated_image(self) -> Image:
+        # TODO: real implementation
+        return self.base_image.image
+
+    # def get_decorated_image(self, decoration: BorderDecorator) -> Image:
+    #     new_img = Image.new(
+    #         "RGB",
+    #         decoration.get_size_with_border(self.image_dims),
+    #         color=decoration.background_color,
+    #     )
+    #     new_img.paste(self._image, decoration.get_border_size(self.image_dims))
+    #     self._draw_text(new_img, decoration)
+    #
+    #     return new_img
+
+    # def _draw_text(self, img: Image, decoration: BorderDecorator) -> None:
+    #     draw = ImageDraw.Draw(img)
+    #     font = self._get_font(decoration)
+    #     border_x, border_y = decoration.get_border_size(self.image_dims)
+    #
+    #     text = self._get_text()
+    #     text_offset_pixel = int(
+    #         (border_x - decoration.get_font_size(self.image_dims)) * 0.5
+    #     )
+    #     text_origin = (
+    #         text_offset_pixel + border_x,
+    #         text_offset_pixel + self.image_dims[1] + border_y,
+    #     )
+    #     draw.text(text_origin, text, fill=decoration.text_color, font=font)
+    #
+    # def _get_text(self) -> str:
+    #     # 50mm | f/2.8 | 1/250s | ISO 400
+    #     exif = PhrugalExifData(self.file_name)
+    #
+    #     candidates = [
+    #         exif.get_focal_len(),
+    #         exif.get_aperture(),
+    #         exif.get_shutter_speed(),
+    #         exif.get_iso(),
+    #     ]
+    #     t = " | ".join([x for x in candidates if x])
+    #     return t
+    #
+    # def _get_font(self, decoration: BorderDecorator) -> ImageFont.FreeTypeFont:
+    #     font_size = int(decoration.get_font_size(self.image_dims))
+    #     if decoration.font is None:
+    #         font = ImageFont.truetype("arial.ttf", size=font_size)
+    #     else:
+    #         font = ImageFont.truetype(decoration.font, size=font_size)
+    #     return font
 
     def get_border_size(self, image_dims: Dimensions) -> Dimensions:
         nominal_dimension_x_mm = 130
--- src/phrugal/composition.py
+++ src/phrugal/composition.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import Iterable
 
+from PIL.Image import Image
+
+import phrugal.border_decoration
 from phrugal.image import PhrugalImage
 
 
@@ -9,7 +12,16 @@ class ImageComposition:
         self.images = images
 
     def write_composition(self, filename: Path):
-        pass
+        decorated_images = self._get_decorated_images()
+        # TODO: arrange images into new image
+        # TODO: write images out
+
+    def _get_decorated_images(self) -> Iterable[Image]:
+        decorated_images = []
+        for image in self.images:
+            decorator = phrugal.border_decoration.BorderDecorator(image)
+            decorated_images.append(decorator.get_decorated_image())
+        return decorated_images
 
     def close_images(self):
         for image in self.images:
