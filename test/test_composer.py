2bb7bdd40c75d5a456456050a77943d5bedd82c5
Martin Demling <0x6d64@martin-demling.de>
2024-08-15 20:14:11 +0300

    update test cases

--- test/test_composer.py
+++ test/test_composer.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from phrugal.composer import PhrugalComposer
+from phrugal.decoration_config import DecorationConfig
 
 
 class TestPhrugalComposer(unittest.TestCase):
@@ -10,19 +11,21 @@ class TestPhrugalComposer(unittest.TestCase):
         self.test_data_path = Path("./img/aspect-ratio")
         self._temp_dir = TemporaryDirectory(prefix="phrugal-test")
         self.temp_path = Path(self._temp_dir.name)
+        self.deco_config = DecorationConfig()
+        self.deco_config.load_default_config()
 
     def tearDown(self):
         self._temp_dir.cleanup()
 
     def test_constructor(self):
-        composer = PhrugalComposer()
+        composer = PhrugalComposer(decoration_config=self.deco_config)
         self.assertAlmostEqual(4.0 / 3.0, composer.target_aspect_ratio)
 
-        composer = PhrugalComposer(target_aspect_ratio=0.1)
+        composer = PhrugalComposer(decoration_config=self.deco_config, target_aspect_ratio=0.1)
         self.assertAlmostEqual(0.1, composer.target_aspect_ratio)
 
     def test_discover_images(self):
-        composer = PhrugalComposer()
+        composer = PhrugalComposer(decoration_config=self.deco_config)
         composer.discover_images(self.test_data_path)
         expected = [
             "img\\aspect-ratio\\100x600.jpg",
@@ -38,6 +41,6 @@ class TestPhrugalComposer(unittest.TestCase):
         self.assertListEqual(expected, [str(x) for x in composer.input_files])
 
     def test_create_composition(self):
-        composer = PhrugalComposer()
+        composer = PhrugalComposer(decoration_config=self.deco_config)
         composer.discover_images(self.test_data_path)
         composer.create_compositions(output_path=self.temp_path, images_count=2)
