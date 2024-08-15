dd3bde1baa2302cba491aa6889545036487c3d91
Martin Demling <0x6d64@martin-demling.de>
2024-08-15 17:37:07 +0300

    make parameters in config work

--- src/phrugal/decoration_config.py
+++ src/phrugal/decoration_config.py
@@ -4,13 +4,14 @@ from .exif import PhrugalExifData
 
 
 class DecorationConfig:
-    def __init__(self, image: Path | str | None = None):
+    def __init__(self, image: Path | str | None = None, item_separator: str = " | "):
         self.bottom_left = []
         self.bottom_right = []
         self.top_left = []
         self.top_right = []
         self.image: Path | str = image
         self._exif: PhrugalExifData | None = None
+        self.item_separator = item_separator
 
     @property
     def exif(self):
@@ -27,7 +28,7 @@ class DecorationConfig:
             ("iso", {}),
         ]
         self.bottom_right = [
-            ("gps_coordinates", {}),
+            ("gps_coordinates", {"use_dms": True}),
         ]
         self.top_left = [
             ("description", None),
@@ -38,26 +39,28 @@ class DecorationConfig:
 
     def get_string_at_corner(self, corner: str) -> str:
         if corner == "bottom_left":
-            result_string = self._get_configured_string(self.bottom_left)
+            result_string = self._build_configured_string(self.bottom_left)
         elif corner == "bottom_right":
-            result_string = self._get_configured_string(self.bottom_right)
+            result_string = self._build_configured_string(self.bottom_right)
         elif corner == "top_left":
-            result_string = self._get_configured_string(self.top_left)
+            result_string = self._build_configured_string(self.top_left)
         elif corner == "top_right":
-            result_string = self._get_configured_string(self.top_right)
+            result_string = self._build_configured_string(self.top_right)
         else:
             raise ValueError(f"Corner name {corner} is not valid")
         return result_string
 
-    def _get_configured_string(self, configured_items: list) -> str:
+    def _build_configured_string(self, configured_items: list) -> str:
         result_fragments = []
-        item_separator = " | "
         for item in configured_items:
             item_name, item_config_params = item
             exif_getter_name = f"get_{item_name}"
             if not hasattr(self.exif, exif_getter_name):
                 raise ValueError(f"item {item_name} not implemented")
             getter = getattr(self.exif, exif_getter_name)
-            single_fragment = getter()
+            if item_config_params:
+                single_fragment = getter(**item_config_params)
+            else:
+                single_fragment = getter()
             result_fragments.append(single_fragment)
-        return item_separator.join((x for x in result_fragments if x))
+        return self.item_separator.join((x for x in result_fragments if x))
