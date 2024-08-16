56e358c1e8d7e496bfbf709afdb1325551d96266
Martin Demling <0x6d64@martin-demling.de>
2024-08-16 09:48:02 +0300

    continue composer

--- src/phrugal/composer.py
+++ src/phrugal/composer.py
@@ -23,26 +23,14 @@ class PhrugalComposer:
         self._img_instances = []
         self.target_aspect_ratio = Fraction(target_aspect_ratio)
 
-    def create_compositions(self, output_path: Path | str, images_count: int):
-        """Does the following:
+    def create_compositions(
+        self,
+        output_path: Path | str,
+        images_count: int,
+        padding_strategy: str = "placeholder",
+    ):
+        self._validate_padding_strategy(padding_strategy)
 
-        - read the image metadata of the input files
-        - figure out the images that go together in a single composition
-        - for each composition: create it, and write the output into the output folder
-        - after that, free the space of that composition
-
-        - For each picture in the input, calculate the aspect ratio
-            - if the aspect ratio is >1, calculate 1/aspect ratio (to normalize it)
-        - sort the images by aspect ratio
-        - do groups for N images
-        - for each of the groups:
-            - do all combinations of orders of images
-            - for each combination: create score for the fit
-                - todo: how can we assess the fit as a score?
-            - then: select best, trigger actual creation of the image
-
-
-        """
         for in_file in self.input_files:
             img = PhrugalImage(in_file)
             self._img_instances.append(img)
@@ -51,7 +39,10 @@ class PhrugalComposer:
             self._img_instances, key=lambda x: x.aspect_ratio_normalized, reverse=True
         )
 
-        img_groups = self.generate_tuples(self._img_instances, images_count)
+        img_groups = self._generate_img_groups(self._img_instances, images_count)
+        self._process_img_group(img_groups, output_path)
+
+    def _process_img_group(self, img_groups, output_path):
         for group in img_groups:
             composition_filename = (
                 output_path / "-".join(x.file_name.stem for x in group)
@@ -62,16 +53,28 @@ class PhrugalComposer:
             composition.write_composition(
                 filename=composition_filename, decoration_config=self.decoration_config
             )
-        print(img_groups)
+
+    @staticmethod
+    def _validate_padding_strategy(padding_strategy):
+        if padding_strategy == "placeholder":
+            pass
+        elif padding_strategy == "duplicate":
+            raise NotImplementedError("not yet implemented")
+        elif padding_strategy == "upscale":
+            raise NotImplementedError("not yet implemented")
+        else:
+            raise RuntimeError(f"don't know padding strategy {padding_strategy}")
 
     def discover_images(self, path):
         self.input_files = [p for p in Path(path).glob("**/*.jpg")]
 
     @staticmethod
-    def generate_tuples(input_objects: List[T], tuple_len: int) -> List[Tuple[T, ...]]:
+    def _generate_img_groups(
+        input_objects: List[T], group_len: int
+    ) -> List[Tuple[T, ...]]:
         """Split a list into tuples of size n (last remainder tuple can be smaller)."""
-        result = list(zip(*[input_objects[i:] for i in range(tuple_len)]))
-        remainder = input_objects[len(result) * tuple_len :]
+        result = list(zip(*[input_objects[i:] for i in range(group_len)]))
+        remainder = input_objects[len(result) * group_len :]
         if remainder:
             result.append(tuple(remainder))
         return result
