9a33d587803dfef2d7a2df1c100264bd90a79933
Martin Demling <0x6d64@martin-demling.de>
2024-08-21 23:35:19 +0300

    change padding

--- src/phrugal/composer.py
+++ src/phrugal/composer.py
@@ -1,12 +1,18 @@
+from enum import StrEnum, unique, auto
 from fractions import Fraction
 from pathlib import Path
-from typing import List, Tuple, TypeVar
+from typing import List, Tuple
 
 from phrugal.composition import ImageComposition
 from phrugal.decoration_config import DecorationConfig
 from phrugal.image import PhrugalImage
 
-T = TypeVar("T")
+
+@unique
+class PaddingStrategy(StrEnum):
+    PLACEHOLDER = auto()
+    DUPLICATE = auto()
+    UPSCALE = auto()
 
 
 class PhrugalComposer:
@@ -20,17 +26,16 @@ class PhrugalComposer:
     ):
         self.decoration_config = decoration_config
         self.input_files = input_files
-        self._img_instances = []
+        self._img_instances: List[PhrugalImage] = []
         self.target_aspect_ratio = Fraction(target_aspect_ratio)
+        self._image_groups: List[Tuple[PhrugalImage, ...]] | None = None
 
     def create_compositions(
         self,
         output_path: Path | str,
         images_count: int,
-        padding_strategy: str = "placeholder",
+        padding_strategy: PaddingStrategy = PaddingStrategy.PLACEHOLDER,
     ):
-        self._validate_padding_strategy(padding_strategy)
-
         for in_file in self.input_files:
             img = PhrugalImage(in_file)
             self._img_instances.append(img)
@@ -38,11 +43,12 @@ class PhrugalComposer:
         self._img_instances = sorted(
             self._img_instances, key=lambda x: x.aspect_ratio_normalized, reverse=False
         )
-        img_groups = self._generate_img_groups(self._img_instances, images_count)
-        self._process_all_img_groups(img_groups, output_path)
+        self._generate_img_groups(self._img_instances, images_count)
+        self._apply_padding_strategy(padding_strategy)
+        self._process_all_img_groups(output_path)
 
-    def _process_all_img_groups(self, img_groups, output_path):
-        for idx, group in enumerate(img_groups):
+    def _process_all_img_groups(self, output_path):
+        for idx, group in enumerate(self._image_groups):
             composition_filename = output_path / self._get_filename(group, idx)
             composition = ImageComposition(
                 group, target_aspect_ratio=self.target_aspect_ratio
@@ -55,27 +61,25 @@ class PhrugalComposer:
         fn = Path(f"img-{idx}")
         return fn.with_suffix(".jpg")
 
-    @staticmethod
-    def _validate_padding_strategy(padding_strategy):
-        if padding_strategy == "placeholder":
-            pass
-        elif padding_strategy == "duplicate":
-            raise NotImplementedError("not yet implemented")
-        elif padding_strategy == "upscale":
-            raise NotImplementedError("not yet implemented")
+    def _apply_padding_strategy(self, strategy: PaddingStrategy):
+        if strategy == PaddingStrategy.UPSCALE:
+            pass  # do nothing, upscaling happens automatically
+        elif strategy == PaddingStrategy.PLACEHOLDER:
+            raise NotImplementedError()  # fixme: implement!
+        elif strategy == PaddingStrategy.DUPLICATE:
+            raise NotImplementedError()  # fixme: implement!
         else:
-            raise RuntimeError(f"don't know padding strategy {padding_strategy}")
+            raise RuntimeError("unknown strategy!")
 
     def discover_images(self, path):
         self.input_files = [p for p in Path(path).glob("**/*.jpg")]
 
-    @staticmethod
     def _generate_img_groups(
-        input_objects: List[T], group_len: int
-    ) -> List[Tuple[T, ...]]:
+        self, input_objects: List[PhrugalImage], group_len: int
+    ) -> None:
         """Split a list into tuples of size n (last remainder tuple can be smaller)."""
-        result = list(zip(*[input_objects[i:] for i in range(group_len)]))
-        remainder = input_objects[len(result) * group_len :]
+        img_grps = list(zip(*[input_objects[i:] for i in range(group_len)]))
+        remainder = input_objects[len(img_grps) * group_len :]
         if remainder:
-            result.append(tuple(remainder))
-        return result
+            img_grps.append(tuple(remainder))
+        self._image_groups = img_grps
