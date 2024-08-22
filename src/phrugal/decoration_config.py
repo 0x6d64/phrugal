b3ec13c6432e716a686a84a47bc0399ac6688368
Martin Demling <0x6d64@martin-demling.de>
2024-08-22 12:39:15 +0300

    move to dict/json based config

--- src/phrugal/decoration_config.py
+++ src/phrugal/decoration_config.py
@@ -1,34 +1,64 @@
+import json
+from pathlib import Path
+
 from .exif import PhrugalExifData
 
 
 class DecorationConfig:
+    DEFAULT_CONFIG = {
+        "bottom_left": {
+            "focal_length": {},
+            "aperture": {},
+            "shutter_speed": {"use_nominal_value": True},
+            "iso": {},
+        },
+        "bottom_right": {
+            "gps_coordinates": {"use_dms": True},
+        },
+        "top_left": {
+            "description": {},
+        },
+        "top_right": {
+            "timestamp": {"format": "%Y-%m-%dT%H:%M"},
+            "geocode": {"zoom": 12},
+        },
+    }
+
     def __init__(self, item_separator: str = " | "):
-        self.bottom_left = []
-        self.bottom_right = []
-        self.top_left = []
-        self.top_right = []
         self.item_separator = item_separator
+        self._config = dict()
+
+    @property
+    def bottom_left(self):
+        return self._config.get("bottom_left")
+
+    @property
+    def bottom_right(self):
+        return self._config.get("bottom_right")
+
+    @property
+    def top_left(self):
+        return self._config.get("top_left")
+
+    @property
+    def top_right(self):
+        return self._config.get("top_right")
+
+    def load_from_file(self, config_file: Path | str):
+        with open(config_file, "r") as cf:
+            self._config = json.load(cf)
+
+    def write_default_config(self, config_file: Path | str):
+        self._write_config(config_file, self.DEFAULT_CONFIG)
+
+    @staticmethod
+    def _write_config(config_file: Path | str, config: dict):
+        with open(config_file, "w") as cf:
+            json.dump(config, cf, indent=4)
 
     def load_default_config(self):
         """Some default values, created mostly for debug purposes."""
-        self.bottom_left = [
-            # ("camera_model", {}),
-            # ("lens_model", {}),
-            ("focal_length", {}),
-            ("aperture", {}),
-            ("shutter_speed", {"use_nominal_value": True}),
-            ("iso", {}),
-        ]
-        self.bottom_right = [
-            ("gps_coordinates", {"use_dms": True}),
-        ]
-        self.top_left = [
-            ("description", None),
-        ]
-        self.top_right = [
-            ("timestamp", {"format": "%Y-%m-%dT%H:%M"}),
-            ("geocode", {"zoom": 12}),
-        ]
+        self._config = self.DEFAULT_CONFIG
 
     def get_string_at_corner(self, exif: PhrugalExifData, corner: str) -> str:
         if corner == "bottom_left":
@@ -44,10 +74,10 @@ class DecorationConfig:
         return result_string
 
     def _build_configured_string(
-        self, exif: PhrugalExifData, configured_items: list
+        self, exif: PhrugalExifData, configured_items: dict
     ) -> str:
         result_fragments = []
-        for item in configured_items:
+        for item in configured_items.items():
             item_name, item_config_params = item
             exif_getter_name = f"get_{item_name}"
             if not hasattr(exif, exif_getter_name):
