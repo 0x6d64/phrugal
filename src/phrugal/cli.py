1e98bc36b6108f319d0eb52763e131eb44640315
Martin Demling <0x6d64@martin-demling.de>
2024-08-23 17:27:16 +0300

    implement writing of default config

--- src/phrugal/cli.py
+++ src/phrugal/cli.py
@@ -1,5 +1,54 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import phrugal
+
+
+def _get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="phrugal")
+    parser.add_argument("-c", "--config", help="Path to custom JSON config")
+    parser.add_argument(
+        "-i",
+        "--input-dir",
+        help="Path where the tool will recursively look for images to process.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        help="Path to folder where to locate the output. If omitted, will default to current dir. "
+        "The tool will attempt to create a directory if its not yet existing.",
+    )
+    parser.add_argument(
+        "--create-default-config",
+        help="If given, create default configuration at given path (or in current directory if omitted) and exit.",
+        nargs="?",
+        const="",  # the value of no path is given
+        default=None,
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"{parser.prog} {phrugal.__version__}"
+    )
+    return parser
+
+
+def _phrugal_main(args: argparse.Namespace):
+    if args.create_default_config is not None:
+        if args.create_default_config == "":
+            current_dir = os.getcwd()
+            target_file = Path(current_dir) / "phrugal-default.json"
+        else:
+            target_file = Path(args.create_default_config)
+        pc = phrugal.DecorationConfig()
+        pc.write_default_config(target_file)
+
+
 def run_cli():
-    pass
+    parser = _get_parser()
+    parsed_args = parser.parse_args()
+
+    _phrugal_main(parsed_args)
 
 
 if __name__ == "__main__":
