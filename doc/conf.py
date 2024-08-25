764fc9abf32ef401a0d84c4d353990b12c728edf
Martin Demling <0x6d64@martin-demling.de>
2024-08-25 12:56:36 +0300

    make sphinx conf ready for rtd change

--- doc/conf.py
+++ doc/conf.py
@@ -5,41 +5,48 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import phrugal
 
-project = 'phrugal'
-copyright = '2024, Martin Demling'
-author = 'Martin Demling'
+project = "phrugal"
+copyright = "2024, Martin Demling"
+author = "Martin Demling"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx_rtd_theme",
-    "sphinx.ext.autosectionlabel"
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# The short X.Y version.
-import phrugal
+
 version = phrugal.__version__
 # The full version, including alpha/beta/rc tags.
 release = version
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_user": "0x6d64", # Username
-    "github_repo": "phrugal", # Repo name
-    "github_version": "master", # Version
-    "conf_py_path": "/doc/", # Path in the checkout to the docs root
+    "display_github": True,  # Integrate GitHub
+    "github_user": "0x6d64",  # Username
+    "github_repo": "phrugal",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/doc/",  # Path in the checkout to the docs root
 }
 html_logo = "./img/phrugal-logo-draft-small.png"
+
+
+# for readthedocs, see https://about.readthedocs.com/blog/2024/07/addons-by-default/
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
