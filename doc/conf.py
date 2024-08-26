3a29cc4dd14cc5e23dcbdf9c79fd2fb65c3561ba
Martin Demling <0x6d64@martin-demling.de>
2024-08-26 15:00:25 +0300

    add favicons

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,10 @@ author = "Martin Demling"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autosectionlabel",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -28,9 +31,9 @@ release = version
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
 html_theme = "alabaster"
 html_static_path = ["_static"]
+html_favicon = '_static/favicons/favicon-16x16.png'
 
 html_context = {
     "display_github": True,  # Integrate GitHub
